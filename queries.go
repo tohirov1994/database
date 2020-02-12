c92773b890f27dc2362f841caf278436b010665a
BT <BT@mail.ru>
2020-02-12 17:51:14 +0500

    feat: added query to check Id client

--- queries.go
+++ queries.go
@@ -11,6 +11,7 @@ const InsertClientCard = `INSERT INTO clients_cards(pan, pin, balance, holderNam
 const GetLastPAN = `SELECT ifnull(max(pan),0) FROM clients_cards;`
 const InsertService = `INSERT INTO services(service, balance) VALUES (:serviceName, :serviceBalance);`
 const InsertAtm = `INSERT INTO atms(city, district, street) VALUES (:cityName, :districtName, :streetName);`
+const CheckIdClient = `SELECT id FROM clients;`
 
 ///////////////////////////////////// queries for Client ///////////////////////////////////////////////////
 const GetCards = `SELECT id, pan, pin, balance, holderName, cvv, validity FROM clients_cards WHERE client_id = :idClient;`
